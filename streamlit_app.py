15d0fc89f29429a7370fa558b4b346505992dfbb
sarveshwar512 <50966773+sarveshwar512@users.noreply.github.com>
2024-03-14 13:08:10 +0530

    Update streamlit_app.py
    added pandas lib loc fxn and provided a search_on column to search on

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,10 @@ cnx=st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_Name'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
+# st.stop()
+
+pd_df=my_dataframe.to_pandas()
+# st.dataframe(pd_df)
 
 ingredients_List=st.multiselect(
     'Choose up to 5 ingredients: ',
@@ -34,6 +38,10 @@ if ingredients_List:
 
     for fruits_chosen in ingredients_List:
         ingredients_string +=fruits_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+        
         st.subheader(fruits_chosen + ' Nutrition Information')
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruits_chosen)
         fv_df=st.dataframe(data=fruityvice_response.json(),use_container_width=True)
